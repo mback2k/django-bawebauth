19605bd2f20f75e3f298d8fff58347186e4ceff7
Marc Hoersken <info@marc-hoersken.de>
2011-11-07 21:18:48 +0100

    Added basic celery tasks

---
+++ bawebauth/tasks.py
@@ -0,0 +1,17 @@
+from celery.decorators import task
+from bawebauth.models import Device, Usage
+
+@task()
+def task_start_worker():
+    for device in Device.objects.all():
+    	task_reduce_device.apply_async(args=[device.id])
+
+@task()
+def task_reduce_device(device_id):
+    try:
+        Device = Device.objects.get(id=device_id)
+        
+        
+        
+    except Device.DoesNotExist, e:
+        pass
