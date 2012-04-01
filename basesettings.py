e601998d6167390cd90f2dadb99840f1fc7c84bf
Marc Hoersken <info@marc-hoersken.de>
2012-04-01 20:40:49 +0200

    Added registration templates and password management

--- basesettings.py
+++ basesettings.py
@@ -80,17 +80,16 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    
+
     'djcelery',
-    
+
     'yamlcss',
     'jdatetime',
-    
+
     'bawebauth',
+
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
 MIDDLEWARE_CLASSES = (
