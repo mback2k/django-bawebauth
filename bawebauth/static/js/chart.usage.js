d3072d71d50e7a551b05a41cfe8ee50161ed300e
Marc Hoersken <info@marc-hoersken.de>
2012-05-11 18:16:12 +0200

    Replaced Google Charts API with Highcharts

--- bawebauth/static/js/chart.usage.js
+++ bawebauth/static/js/chart.usage.js
@@ -1,14 +1,81 @@
 $(document).ready(function() {
   $('.highchart').each(function(index, chart) {
     $.getJSON($(chart).attr('href'), function(json) {
-      chart = new google.visualization.AnnotatedTimeLine(chart);
-      var data = new google.visualization.DataTable();
+      var options = {
+        chart: {
+          renderTo: $(chart).attr('id'),
+          zoomType: 'x',
+          type: 'areaspline'
+        },
+        title: {
+          text: 'Traffic Usage'
+        },
+        xAxis: {
+          type: 'datetime',
+          maxZoom: 300000,
+          dateTimeLabelFormats: { // don't display the dummy year
+            second: '%H:%M:%S',
+            minute: '%H:%M',
+            hour: '%H:%M',
+            day: '%e. %b',
+            week: '%e. %b',
+            month: '%b \'%y',
+            year: '%Y'
+          }
+        },
+        yAxis: {
+          min: 0
+        },
+        plotOptions: {
+          areaspline: {
+            fillOpacity: 0.5,
+            fillColor: {
+              linearGradient: [0, 0, 0, 300],
+            },
+            lineWidth: 1,
+            marker: {
+              enabled: false,
+              states: {
+                hover: {
+                  enabled: true,
+                  radius: 3
+                }
+              }
+            },
+            shadow: false,
+            states: {
+              hover: {
+                lineWidth: 1
+              }
+            }
+          }
+        },
+        series: [
+          {
+            name: 'Send',
+            data: [],
+            fillColor: {
+              stops: [
+                [0, '#4572A7'],
+                [1, 'rgba(0,0,0,0)']
+              ]
+            }
+          },
+          {
+            name: 'Received',
+            data: [],
+            fillColor: {
+              stops: [
+                [0, '#A74472'],
+                [1, 'rgba(0,0,0,0)']
+              ]
+            }
+          }
+        ]
+      };
+
       var data_latest = new Date(0);
       var data_previous = null;
-
-      data.addColumn('datetime', 'Date');
-      data.addColumn('number', 'Send');
-      data.addColumn('number', 'Received');
 
       $.each(json, function(key, value) {
         var data_crdate = new Date(); data_crdate.setISO8601(value.fields.crdate.replace(/ /, 'T'));
@@ -18,11 +85,16 @@ $(document).ready(function() {
           var data_fix2 = new Date(data_crdate.getTime());
           data_fix1.setMinutes(data_previous.getMinutes()+5);
           data_fix2.setMinutes(data_crdate.getMinutes()-5);
-          data.addRows([[data_fix1, 0, 0]]);
-          data.addRows([[data_fix2, 0, 0]]);
+
+          options.series[0].data.push([data_fix1.getTime(), 0]);
+          options.series[1].data.push([data_fix1.getTime(), 0]);
+          options.series[0].data.push([data_fix2.getTime(), 0]);
+          options.series[1].data.push([data_fix2.getTime(), 0]);
         }
         data_previous = new Date(data_crdate.getTime());
-        data.addRows([[data_crdate, value.fields.send, value.fields.received]]);
+
+        options.series[0].data.push([data_crdate.getTime(), value.fields.send]);
+        options.series[1].data.push([data_crdate.getTime(), value.fields.received]);
       });
 
       var date_start = new Date(data_latest.getTime());
@@ -30,12 +102,7 @@ $(document).ready(function() {
 
       date_start.setDate(date_end.getDate()-1);
 
-      chart.draw(data, {
-        'allowRedraw': true,
-        'scaleType': 'maximized',
-        'zoomStartTime': date_start,
-        'zoomEndTime': date_end,
-      });
+      new Highcharts.Chart(options);
     });
   });
 });
